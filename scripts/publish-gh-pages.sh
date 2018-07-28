f668d421e6a443cb76b8321275daa1efe54625f0
Dmitry Morozov <dimemorozovy@yandex.ru>
2018-07-28 09:53:09 +0300

    update publish script to gh-pages

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -1,2 +1,15 @@
+git checkout master
+git checkout -b tmp-gh-pages
+rimraf .gitignore
+
+npm install
 npm run build:docs
-git checkout gh-pages
+
+git add --all docs/
+git commit -am 'update docs'
+
+git subtree split --prefix docs -b gh-pages
+git push -f origin gh-pages:gh-pages
+git checkout master
+git branch -D tmp-gh-pages
+git branch -D gh-pages
