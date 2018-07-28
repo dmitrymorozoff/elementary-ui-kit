597b103130670c543f0cca482b9abc3c231cd164
Dmitry Morozov <dimemorozovy@yandex.ru>
2018-07-28 08:52:11 +0300

    add events to modal, create sh script

--- src/docs/index.js
+++ src/docs/index.js
@@ -22,20 +22,35 @@ class App extends React.Component {
         const { open } = this.state;
         return (
             <React.Fragment>
-                <Button onClick={this.handleClickOpen.bind(this)}>Open Modal</Button>
-                <Modal open={open} onClose={this.handleClose.bind(this)}>
+                <Button onClick={this.handleClickOpen.bind(this)}>
+                    Open Modal
+                </Button>
+                <Modal
+                    className="my-modal"
+                    closeOnEsc={true}
+                    open={open}
+                    onClose={this.handleClose.bind(this)}
+                >
                     <ModalContent>
-                        Determine the max width of the dialog. The dialog width grows with the size of the screen, this
-                        property is useful on the desktop where you might need some coherent different width size across
-                        your application. Set to false to disable maxWidth.Determine the max width of the dialog. The
-                        dialog width grows with the size of the screen, this property is useful on the desktop where you
-                        might need some coherent different width size across your application. Set to false to disable
-                        maxWidth.Determine the max width of the dialog. The dialog width grows with the size of the
-                        screen, this property is useful on the desktop where you might need some coherent different
-                        width size across your application. Set to false to disable maxWidth.Determine the max width of
-                        the dialog. The dialog width grows with the size of the screen, this property is useful on the
-                        desktop where you might need some coherent different width size across your application. Set to
-                        false to disable maxWidth.
+                        Determine the max width of the dialog. The dialog width
+                        grows with the size of the screen, this property is
+                        useful on the desktop where you might need some coherent
+                        different width size across your application. Set to
+                        false to disable maxWidth.Determine the max width of the
+                        dialog. The dialog width grows with the size of the
+                        screen, this property is useful on the desktop where you
+                        might need some coherent different width size across
+                        your application. Set to false to disable
+                        maxWidth.Determine the max width of the dialog. The
+                        dialog width grows with the size of the screen, this
+                        property is useful on the desktop where you might need
+                        some coherent different width size across your
+                        application. Set to false to disable maxWidth.Determine
+                        the max width of the dialog. The dialog width grows with
+                        the size of the screen, this property is useful on the
+                        desktop where you might need some coherent different
+                        width size across your application. Set to false to
+                        disable maxWidth.
                     </ModalContent>
                 </Modal>
             </React.Fragment>
--- src/lib/components/others/Modal/index.js
+++ src/lib/components/others/Modal/index.js
@@ -40,21 +40,49 @@ const ModalItem = styled.div`
     z-index: 2;
     border: 1px solid #f2f2f5;
     text-align: left;
-    box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 20px -15px, rgba(0, 0, 0, 0.15) 0px 55px 50px -35px;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 20px -15px,
+        rgba(0, 0, 0, 0.15) 0px 55px 50px -35px;
     transition: 0.35s;
-    transform: ${props => (props.open ? "translateY(0)" : "translateY(100%)")};
+    transform: ${props => (props.open ? "scale(1)" : "scale(1.2)")};
 `;
 
 export class Modal extends React.Component {
     constructor(props) {
         super(props);
+        this.overlay = React.createRef();
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener("keydown", this.handleKeyUp, true);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("keydown", this.handleKeyUp, true);
+    }
+
+    handleKeyUp(event) {
+        const { closeOnEsc, onClose } = this.props;
+        const KEY_CODE_ESC = 27;
+        if (closeOnEsc && event.keyCode === KEY_CODE_ESC) {
+            onClose();
+        }
+    }
+
+    handleClose(event) {
+        const { onClose } = this.props;
+        if (this.overlay.current !== event.target) {
+            return;
+        }
+        onClose();
     }
 
     render() {
-        const { open, children } = this.props;
+        const { open, children, className } = this.props;
         return (
-            <Outer open={open}>
-                <Overlay>
+            <Outer open={open} tabIndex="-1" className={className}>
+                <Overlay onClick={this.handleClose} innerRef={this.overlay}>
                     <ModalItem open={open}>{children}</ModalItem>
                 </Overlay>
             </Outer>
@@ -64,10 +92,13 @@ export class Modal extends React.Component {
 
 Modal.defaultProps = {
     open: false,
+    closeOnEsc: false,
 };
 
 Modal.propTypes = {
     children: PropTypes.node,
     onClose: PropTypes.func.isRequired,
     open: PropTypes.bool.isRequired,
+    closeOnEsc: PropTypes.bool,
+    className: PropTypes.string,
 };
